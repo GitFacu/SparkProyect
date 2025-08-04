cd866798fedac8963bda3dc21b92840cc0d4d068
GitFacu <hanfacundosolo@gmail.com>
2025-08-04 03:35:24 -0300

    Collider spawn

---
+++ Assets/Scripts/FallCollider.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallCollider : MonoBehaviour
+{
+    //[SerializeField] private Transform _spawnPos;
+
+    //Private void ontriggerenter(collider other)
+    //{
+    //    if (other, getcomponent<Player>()) 
+    //    {
+    //        Other.getxomponent<Player>().ResetPosition(_spawnPoint)
+    //    }
+
+    //}
+
+
+
+
+
+}
